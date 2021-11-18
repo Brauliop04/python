e2c4e62a58133f538dbd7dee54c75b209b0be3bf
Brauliop04 <94642663+Brauliop04@users.noreply.github.com>
2021-11-18 13:58:40 -0600

    uploaded the lists code

---
+++ lists prgm.py
@@ -0,0 +1,25 @@
+# Tuesday Nov. 16
+# Braulio Pacheco Romo 
+# Create list
+
+inventory = ["Bike", "Joggers", "Pistol", "Machette", "Keys", "Wallet"]
+
+# while loop
+
+things = 0
+
+while things < len(inventory):
+  print(inventory[things])
+  things = things + 1
+
+print("^^^^While loop^^^^")
+
+# for loop
+
+for things in inventory:
+    print(things)
+
+
+print("^^^^for loop^^^^")
+
+
